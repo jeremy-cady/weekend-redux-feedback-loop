6d35b1c6617ae9760157dae8d3bbe1e5d365e0b7
jeremy-cady <jeremy.cady@outlook.com>
2022-01-24 11:10:38 -0600

    added dropdown menus for number input

--- src/components/FeelingsForm/FeelingsForm.jsx
+++ src/components/FeelingsForm/FeelingsForm.jsx
@@ -24,14 +24,29 @@ function FeelingsForm() {
     return(
         <>
             <h1>How are you feeling today?</h1>
-            <input 
-                type="number"
+            <select 
+                onChange={(event) => setFeelingsInput(event.target.value)}
+                value={feelingsInput}>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                
+            </select> 
+                {/* type="number"
                 min="0"
                 max="10"
                 step="1"
                 onChange={(event) => setFeelingsInput(event.target.value)}
                 value={feelingsInput}
-            />
+            /> */}
             <button onClick={onSubmit}>NEXT</button>
         </>
     )
