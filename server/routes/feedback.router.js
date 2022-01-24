066974571c97856c810e9a29d172c64c3a7e1cfb
jeremy-cady <jeremy.cady@outlook.com>
2022-01-24 11:31:40 -0600

    added a /GET route to test Postman

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
+//GET Route
+router.get('/', (req, res) => {
+    const queryText = `
+        SELECT * FROM "feedback";
+    `;
+
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log('GET /feedback error', error);
+            res.sendStatus(500);        
+        });
+})
+
+
 // POST route
 router.post('/', (req, res) => {
     console.log(req.body);
